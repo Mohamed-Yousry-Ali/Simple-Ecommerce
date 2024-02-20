34da72eb672da5bdb1c502811677915c08005049
Mohamed-Yousry-Ali <mohamedyousry631@gmail.com>
2024-02-20 16:37:10 +0200

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h4 className="text-3xl font-bold underline">
+      Hello world!
+    </h4>
+
     </>
   )
 }
